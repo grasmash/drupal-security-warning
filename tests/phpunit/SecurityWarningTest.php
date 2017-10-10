7b61de3cf1fb03bf68c5fcf4ecbb15e72062e318
Matthew Grasmick <matt.grasmick@gmail.com>
2017-10-09 22:21:16 -0400

    Add negative assertion.

--- tests/phpunit/SecurityWarningTest.php
+++ tests/phpunit/SecurityWarningTest.php
@@ -26,8 +26,9 @@ class SecurityWarningTest extends TestCase
     $output = $process->getOutput();
 
     $this->assertContains("You are using Drupal packages that are not supported by the Drupal Security Team!", $output);
-    $this->assertContains("drupal/ctools:3.0.0.0-alpha27: Alpha releases are not covered by Drupal security advisories.", $output);
+    $this->assertContains("- drupal/ctools:3.0.0.0-alpha27: Alpha releases are not covered by Drupal security advisories.", $output);
     $this->assertContains("See https://www.drupal.org/security-advisory-policy for more information.", $output);
+    $this->assertNotContains("- drupal/token:1.0.0", $output);
   }
 
 }
